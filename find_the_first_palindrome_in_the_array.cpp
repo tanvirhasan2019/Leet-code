7c7e05d41cce21bfc72cec34712bb9139dbf1166
tanvirhasan2019 <tanvirhasan.tanshen2019@gmail.com>
2023-11-16 13:00:59 +0900

    Find the first palindrom array problem solved

---
+++ find_the_first_palindrome_in_the_array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        for(int i=0; i<words.size(); i++){
+            string str = words[i];
+            std::reverse(str.begin(), str.end());
+            if(str == words[i]) return words[i];
+        }
+        return "";
+    }
+};
