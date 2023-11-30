67ab41ca073f3a530faf38363481408956e93d66
tanvirhasan2019 <tanvirhasan.tanshen2019@gmail.com>
2023-11-30 12:02:24 +0900

    Find words containing charecters

---
+++ find_words_containing_charecters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        std::vector<int>vec;
+        for(int i=0; i<words.size(); i++){
+            for(int j=0; j <words[i].size(); j++){
+                if(words[i][j] == x){
+                    vec.push_back(i);
+                    break;
+                }
+            }
+        }
+        return vec;
+    }
+};
