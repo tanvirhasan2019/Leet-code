259c3985c896cd3d76051fe7d15641ca356fb1b0
tanvirhasan2019 <tanvirhasan.tanshen2019@gmail.com>
2024-01-09 09:42:27 +0900

    Lexicographically smallest palindrome

---
+++ legicographicallY_smallest_palindrome.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int l=0, r=s.size()-1;
+        while(l<r){
+            if(s[l] > s[r]) s[l] = s[r];
+            else s[r] = s[l];
+            l++;
+            r--;
+        }
+        return s;
+    }
+};
